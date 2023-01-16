b0aae412341960889766eeb7f35708b23822ffd1
bianca <me@biancarosa.com.br>
2023-01-16 20:07:21 -0300

    feat: add healthcheck

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/nomadphone/message-router/webhooks"
 )
 
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
+}
+
 func main() {
+	http.HandleFunc("/health-check", healthCheck)
 	http.HandleFunc("/twillio/sms", webhooks.ReceiveSMS)
 	port := os.Getenv("PORT")
 	if port == "" {
